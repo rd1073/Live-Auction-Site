bf686c20a2ad1e633e610f3effa5764a0935a56e
rd1073 <rd1073@srmist.edu.in>
2023-12-02 12:02:47 +0530

    login register component setup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom'; // Assuming you're using React Router for navigation
+
+
+
+
 
 const Login = () => {
-    const [userName, setUserName] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+
+    };
+
 
   return (
     <div>
-      <form className="home__form" onSubmit={handleSubmit}>
-        <label htmlFor="username">Enter your username</label>
-        <input
-          type="text"
-          name="username"
-          className="home__input"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-          required
-          minLength={6}
-        />
-        <button className="home__cta">SIGN IN</button>
-      </form>
-    </div>
+    <h2>Login</h2>
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button type="button" onClick={handleLogin}>
+        Login
+      </button>
+    </form>
+    <p>
+      Not a user? <Link to="/register">Register here</Link>.
+    </p>
+  </div>
     
   )
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'; // Assuming you're using React Router for navigation
+
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleRegister = async () => {
+
+
+
+  };
+
+
+
   return (
     <div>
-      
+      <h2>Register</h2>
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+         
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleRegister}>
+          Register
+        </button>
+      </form>
+      <p>
+        Already have an account? <Link to="/login">Login here</Link>.
+      </p>
     </div>
   )
 }
